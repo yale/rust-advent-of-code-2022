379d20fe8e6329042447f0c723747086271db1ec
Yale Spector <yspector@hellobrightline.com>
2022-12-01 12:32:15 -0500

    bake input string into binary

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,17 +1,14 @@
-use std::fs;
-
 fn main() {
-    let contents = fs::read_to_string("input.txt").expect("Failed to read file");
+    let contents = include_str!("input.txt");
 
     let elves = contents.trim().split("\n\n");
 
     let mut calories_per_elf: Vec<i32> = elves.map(|calories_group| {
-        calories_group.split("\n").map(|calories| {
-            match calories.parse::<i32>() {
-                Ok(n) => n,
-                Err(_) => panic!("Invalid input!")
-            }
-        }).sum()
+        calories_group
+            .split("\n")
+            .map(str::parse::<i32>)
+            .map(Result::unwrap)
+            .sum()
     }).collect();
 
     calories_per_elf.sort();
